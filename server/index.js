ec81f8782b208577110d39af057ec695e1d0df93
JLopezz3 <58873590+JLopezz3@users.noreply.github.com>
2020-12-20 15:56:35 -0600

    sdsa

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const db = require("./db/db");
 // const bodyParser = require('body-parser');
 // const cors = require('cors');
 const app = express();
-const PORT = 3001;
+const PORT = 8080;
 
 
 // app.use(cors());
