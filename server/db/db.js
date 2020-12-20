9f16a54999eca0e4cd08efef94459330b04559e9
trinhamcity1 <trinhamcity1@gmail.com>
2020-12-20 14:55:33 -0600

    yo boi

--- server/db/db.js
+++ server/db/db.js
@@ -3,7 +3,7 @@ const db = mysql.createConnection({
     host: "us-cdbr-east-02.cleardb.com",
     user: "bbcb0c824a3386",
     password:"a921fcf7",
-    database: "heroku_fcf45fc874be4c6",
+    database: "mxer",
 
 
 })
